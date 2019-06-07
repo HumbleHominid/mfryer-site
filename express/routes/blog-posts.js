d9b5977b07593321e68c899781ca31d5d78e4a3c
Michael Fryer <micheal.d.fryer@gmail.com>
2019-06-07 12:48:42 -0700

    refactored the blogpost stuff to use pure markdown rather than markdown in json

--- express/routes/blog-posts.js
+++ express/routes/blog-posts.js
@@ -9,15 +9,14 @@ function badRequest(req, res) {
 
 router.route('/blog-post/:blog_id')
 .get((req, res) => {
-    fs.readFile(`./data/blog-posts/${req.params.blog_id}.json`, (err, data) => {
+    fs.readFile(`./data/blog-posts/${req.params.blog_id}.md`, (err, data) => {
         if (err) {
             res.sendStatus(500);
             return;
         }
 
         res.set('Access-Control-Allow-Origin', '*');
-        let blog_data = JSON.parse(data);
-        res.status(200).json({ content: blog_data.markdown, title: blog_data.title });
+        res.status(200).json({ content: data.toString() });
     });
 })
 .all(badRequest);
@@ -25,17 +24,20 @@ router.route('/blog-post/:blog_id')
 router.route('/blog-post')
 .get((req, res) => {
     fs.readdir('./data/blog-posts', (err, data) => {
-        let blog_titles = [ ];
+        let blog_data = [ ];
         
         data.sort().reverse().forEach((filename) => {
-            if (filename.match(/json$/i)) {
-                let blog_data = JSON.parse(fs.readFileSync(`./data/blog-posts/${filename}`));
-                blog_titles.push({ file: filename.replace(/\.json$/i, ''), title: blog_data.title });
+            if (filename.match(/md$/i)) {
+                let blog_post = fs.readFileSync(`./data/blog-posts/${filename}`).toString();
+                blog_data.push({
+                    file: filename.replace(/\.md$/i, ''),
+                    title: blog_post.substring(2, blog_post.indexOf('\n'))
+                });
             }
         });
 
         res.set('Access-Control-Allow-Origin', '*');
-        res.status(200).json({ content: blog_titles });
+        res.status(200).json({ content: blog_data });
     })
 })
 .all(badRequest);
