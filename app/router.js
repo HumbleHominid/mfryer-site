756be707bc9aefb38f8924e27ccc1ca3a9297055
Michael Fryer <michael.d.fryer@gmail.com>
2019-06-14 16:59:24 -0700

    fixed routing issue

--- app/router.js
+++ app/router.js
@@ -8,7 +8,9 @@ const Router = EmberRouter.extend({
 
 Router.map(function() {
   this.route('about');
-  this.route('blogs');
+  this.route('blogs', function() {
+    this.route('blog', { path: '/:blog_id' });
+  });
   this.route('game');
   this.route('error-page');
 });
